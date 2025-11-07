fdc739a5efde72bd6127402be85ae316f893136a
8ouoouo8 <ouovoout@outlook.com>
2025-11-07 16:21:47 +0800

    Add files via upload

---
+++ 第一周/第三题-matplotlab/1t3.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+plt.plot(x, y, linestyle='--')
+plt.xlim(0, 10)
+plt.ylim(-1.5, 1.5)
+plt.grid(True)
+plt.show()
